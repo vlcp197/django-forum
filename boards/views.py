e3ac5ead85aea4ff7154168324f6015f8da6aabf
vlcp197 <vlcp197@gmail.com>
2023-11-17 17:52:35 -0300

    Followed the tutorial until the second part

---
+++ boards/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from models import Board
+
+
+def home(request):
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
