385cfe714318769395cb3e077a5aa978ec46fd23
vlcp197 <vlcp197@gmail.com>
2023-11-18 01:47:15 -0300

    Create view board_topics and add it to urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, re_path
 from boards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     re_path(r'^$', views.home, name='home'),
+    re_path(
+        r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    path('admin/', admin.site.urls),
 ]
