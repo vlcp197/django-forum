9ae58720077c45665754c0c2e11f70c03da03bb8
vlcp197 <vlcp197@gmail.com>
2023-11-23 16:44:01 -0300

    Add app widget_tweaks to the project

--- myproject/settings.py
+++ myproject/settings.py
@@ -16,7 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards'
+    'boards',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
